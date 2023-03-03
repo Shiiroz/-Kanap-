171458fbb7fa9be311df49e1a891dea59425cce2
Ryan Mortuaire <ryanmortuaire@gmail.com>
2023-03-03 19:45:51 +0100

    exercice du projet

---
+++ front/js/script.js
@@ -0,0 +1,28 @@
+const url = "http://localhost:3000/api/products"; //Recuperation du Catalogue de canapés
+const catalogue = document.getElementById("items");
+
+async function getApiCatalog() {
+    let catalog = {}
+
+    await fetch(url)
+    .then((response) => response.json())
+    .then ((data) => {catalog = data})
+
+    return catalog;
+}
+    
+function buildHtml(elements) {
+            console.log(elements)
+        elements.forEach(element => {
+            catalogue.innerHTML += `<a href="./product.html?id= ` + element._id + `">
+            <article>
+            <img src="${element.imageUrl} " alt="${element.alttxt} ">
+                <h3 class="productName">${element.name}</h3>
+            <p class="productDescription">${element.description} </p>
+            </article>
+        </a>`;
+        });
+}
+
+
+buildHtml(await getApiCatalog());
