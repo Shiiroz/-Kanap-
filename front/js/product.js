b9f1f84c8df2d6e2249ea045826c53d18c3694a5
Ryan Mortuaire <ryanmortuaire@gmail.com>
2023-03-10 19:34:06 +0100

    Ajout de la description

--- front/js/product.js
+++ front/js/product.js
@@ -1,16 +1,55 @@
 const params = new URL(document.location).searchParams;
-const id  = params.get("id");
+const _id  = params.get("_id");
 const url = `http://localhost:3000/api/products/${_id}`;
+console.log(url)
 
 
 const getElement = async () => {
-    try {
-        const response = await fetch(url);
-        const data = await response.json();
-        console.log(data);
-    } catch (error) {
-        console.error(error);
-    }
+    fetch(url)
+    .then(function(response){
+        return response.json()
+    })
+    .then(function(data){
+        console.log(data)
+        const addTitre = (document.getElementById("title").innerHTML= data.name)
+        const addPrix = (document.getElementById("price").innerHTML= data.price)
+        const addImage = (document.createElement("img"))
+        document.querySelector(".item__img").appendChild(addImage)
+        addImage.setAttribute("src", `${data.imageUrl}`)
+
+            // ajout de la description
+        const addDescription = (document.getElementById("description").innerHTML= data.description)
+        const addOption = document.getElementById("colors")
+        for (color in data.colors){
+            addOption.innerHTML += `<option value="${data.colors[color]}">${data.colors[color]}</option>`
+        }
+    })
 }
 
+const addToCart = document.getElementById("addToCart")
+addToCart.addEventListener("click", () =>{
+    const addPro ={
+        quantity : document.getElementById("colors").value,
+        color : document.getElementById("colors").value,
+        id : id
+    }
+})
+
+
 getElement();
+
+
+/*
+1-je veux ajouter au panier 
+  1a-si le panier n'existe pas j veux le cree
+  1b-si il existe je veux le recuperer
+2-je veux ajouter le produit dans le panier
+    2a-si il n'existe pas j'ajoute le produit dans le panier
+    2b-si il exist edans la meme couleur mettre a jour la quantiter
+3-je veux calculer le prix total du panier
+
+mon contenant c'est le panier 
+mon contenue c'est l'article 
+un article est defini par la quantiter couleur et id 
+mon panier definit par une collection d'article et le prix total
+*/
--- front/js/script.js
+++ front/js/script.js
@@ -14,7 +14,7 @@ async function getApiCatalog() {
 function buildHtml(elements) {
             console.log(elements)
         elements.forEach(element => {
-            catalogue.innerHTML += `<a href="./product.html?id= ` + element._id + `">
+            catalogue.innerHTML += `<a href="./product.html?_id=` + element._id + `">
             <article>
             <img src="${element.imageUrl} " alt="${element.alttxt} ">
                 <h3 class="productName">${element.name}</h3>
